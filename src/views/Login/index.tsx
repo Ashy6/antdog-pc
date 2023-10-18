9ee7a83589aa06803b1aeafe2f6bcfa5eb245430
Your zengjunxiao <z1801273437@163.com>
2023-10-19 00:05:50 +0800

    feat: 优化登录页面

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,56 +1,67 @@
-import {Button, Checkbox, Form, Input} from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd'
+import './style.scss'
 
-const onFinish = (values: any) => {
-    console.log('Success:', values);
-};
+export const Login = (): JSX.Element => {
+    const onFinish = (values: unknown) => {
+        console.log('Success:', values)
+    }
 
-const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-};
+    const onFinishFailed = (errorInfo: unknown) => {
+        console.log('Failed:', errorInfo)
+    }
 
-type FieldType = {
-    username?: string;
-    password?: string;
-    remember?: string;
-};
+    type FieldType = {
+        username?: string
+        password?: string
+        remember?: string
+    }
 
-export const Login = (): JSX.Element => (
-    <Form
-        name="basic"
-        labelCol={{span: 8}}
-        wrapperCol={{span: 16}}
-        style={{maxWidth: 600}}
-        initialValues={{remember: true}}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off">
-        <Form.Item<FieldType>
-            label="Username"
-            name="username"
-            rules={[{required: true, message: 'Please input your username!'}]}>
-            <Input/>
-        </Form.Item>
+    return (
+        <div className='login-container'>
+            <div className='bg-white text-[#333]'>
+                <label>Hello,Welcome to Antdog</label>
+                <Form
+                    className='login-container-form'
+                    name='basic'
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    style={{ maxWidth: 600 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete='off'
+                >
+                    <Form.Item<FieldType>
+                        label='Username'
+                        name='username'
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
 
-        <Form.Item<FieldType>
-            label="Password"
-            name="password"
-            rules={[{required: true, message: 'Please input your password!'}]}
-        >
-            <Input.Password/>
-        </Form.Item>
+                    <Form.Item<FieldType>
+                        label='Password'
+                        name='password'
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
 
-        <Form.Item<FieldType>
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{offset: 8, span: 16}}
-        >
-            <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+                    <Form.Item<FieldType>
+                        name='remember'
+                        valuePropName='checked'
+                        wrapperCol={{ offset: 8, span: 16 }}
+                    >
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
 
-        <Form.Item wrapperCol={{offset: 8, span: 16}}>
-            <Button type="primary" htmlType="submit">
-                Submit
-            </Button>
-        </Form.Item>
-    </Form>
-);
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type='primary' htmlType='submit'>
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    )
+}
