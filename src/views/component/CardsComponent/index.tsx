cc9a749224cf0dde8e2aab6709c4778596890ac6
Your zengjunxiao <z1801273437@163.com>
2023-11-01 17:57:39 +0800

    chore: 调整详情页面样式

--- src/views/component/CardsComponent/index.tsx
+++ src/views/component/CardsComponent/index.tsx
@@ -49,8 +49,8 @@ const CardsComponent = (props: {
         'https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp',
         'https://gw.alipayobjects.com/zos/antfincdn/cV16ZqzMjW/photo-1473091540282-9b846e7965e3.webp',
         'https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp',
-        'https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp',
-        'https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp'
+        // 'https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp',
+        // 'https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp'
     ])
     const [opened, setOpen] = useState(false);
     const [rulingOpened, setRulingOpened] = useState(false);
@@ -259,11 +259,11 @@ const CardsComponent = (props: {
         }
     ];
 
-    const componentClass = `card-item ${isDetails ? 'detail-content' : ''}`;
+    const detailClass = `${isDetails ? 'detail-content' : ''}`;
     return (
-        <div className={componentClass}>
+        <div className='card-item'>
             {/* amount */}
-            <div className='card-item-amount cursor-pointer' onClick={openDetails}>
+            <div className={`card-item-amount cursor-pointer ${detailClass}`} onClick={openDetails}>
                 <img className='card-item-icon left' src={icon.steam} alt='' />
                 <div className='amount'>
                     <label className='amount-title'>{advCode}</label>
@@ -281,7 +281,7 @@ const CardsComponent = (props: {
             </div>
 
             {/* order info */}
-            <div className='card-item-order'>
+            <div className={`card-item-order ${detailClass}`} >
                 <div className='card-item-order-info'>
                     <span>Order Number</span>
                     <span>{orderNo}</span>
@@ -296,34 +296,75 @@ const CardsComponent = (props: {
                 </div>
             </div>
 
-            {/* images */}
-            <div className='card-item-images'>
-                <Image.PreviewGroup>
-                    {/* TODO: 这里的样式渲染有问题，考虑每三个一组独立渲染
-                    每一组加 detail-content 类名保证不换行展示*/}
-                    {img.slice(0, isDetails ? img.length : 3).map((img, i) => (
-                        <Image key={i} width={100} height={120} src={img} />
-                    ))}
-                    {/* 不满足三个展示空占位 */}
-                    {img.length < 3 &&
-                        Array.from({ length: 3 - img.length }, (_, i) => (
-                            <div className='ant-image' key={`empty-${i}`} style={{ height: 120 }}></div>
+            <div className={detailClass}>
+                {/* images */}
+                <div className='card-item-images' >
+                    <Image.PreviewGroup>
+                        {/* TODO: 这里的样式渲染有问题，考虑每三个一组独立渲染
+                        每一组加 detail-content 类名保证不换行展示*/}
+                        {img.slice(0, isDetails ? img.length : 3).map((img, i) => (
+                            <Image key={i} width={100} height={120} src={img} />
                         ))}
-                </Image.PreviewGroup>
+                        {/* 不满足三个展示空占位 */}
+                        {img.length < 3 &&
+                            Array.from({ length: 3 - img.length }, (_, i) => (
+                                <div className='ant-image' key={`empty-${i}`} style={{ height: 120 }}></div>
+                            ))}
+                    </Image.PreviewGroup>
+                </div>
+                {/* 图片超过三个显示 */}
+                {img.length > 3 && !isDetails && (
+                    <div className='card-item-more'>······</div>
+                )}
+                {/* USD */}
+                <div className='card-item-USD' >
+                    <span>999999</span>
+                    <span>USD</span>
+                    <span>37793555427155729</span>
+                    <span>05/31</span>
+                    <span>6207</span>
+                </div>
             </div>
 
-            {/* USD */}
-            <div className='card-item-USD'>
-                <span>999999</span>
-                <span>USD</span>
-                <span>37793555427155729</span>
-                <span>05/31</span>
-                <span>6207</span>
-            </div>
+            {selectValue.isRuling ? (<div className='card-item-btn'>
+                <Button className='antdog-btn' type="primary" onClick={showRulingModal}>
+                    Seller Win
+                </Button>
+                <Button className='antdog-btn' type="primary" onClick={showRulingModal}>
+                    Buyer Win
+                </Button>
+            </div>) : (<>
+                {/* In trade */}
+                {orderStatus === OrderStatus.inTrade && <div className='card-item-btn'>
+                    <Button className='antdog-btn' type='primary' onClick={showModal}>
+                        Negotiate
+                    </Button>
+                    <Button className='antdog-btn' type='primary' onClick={submitReleaseOrder}>
+                        Release
+                    </Button>
+                </div>}
 
-            {/* 图片超过三个显示 */}
-            {img.length > 3 && !isDetails && (
-                <div className='card-item-more'>······</div>
+                {/* In dispute */}
+                {[OrderStatus.inDisputeNegotiate].includes(orderStatus) && <div className='card-item-btn short'>
+                    <Button className='antdog-btn disabled' type="primary" disabled>
+                        In dispute
+                    </Button>
+                </div>}
+
+                {/* wait */}
+                {orderStatus === OrderStatus.inDisputeArbitration && <div className='card-item-btn'>
+                    <Button className='antdog-btn disabled' type="primary" disabled>
+                        Platform is in arbitration,please wait
+                    </Button>
+                </div>}
+
+                {/* Completed */}
+                {orderStatus === OrderStatus.completed && <div className='card-item-btn short'>
+                    <Button className='antdog-btn disabled' type="primary" disabled>
+                        Order Completed
+                    </Button>
+                </div>}
+            </>
             )}
 
             <Modal
@@ -383,47 +424,6 @@ const CardsComponent = (props: {
                     })
                 }
             </Modal>
-            {selectValue.isRuling ? (<div className='card-item-btn'>
-                <Button className='antdog-btn' type="primary" onClick={showRulingModal}>
-                    Seller Win
-                </Button>
-                <Button className='antdog-btn' type="primary" onClick={showRulingModal}>
-                    Buyer Win
-                </Button>
-            </div>) : (<>
-                {/* In trade */}
-                {orderStatus === OrderStatus.inTrade && <div className='card-item-btn'>
-                    <Button className='antdog-btn' type='primary' onClick={showModal}>
-                        Negotiate
-                    </Button>
-                    <Button className='antdog-btn' type='primary' onClick={submitReleaseOrder}>
-                        Release
-                    </Button>
-                </div>}
-
-                {/* In dispute */}
-                {[OrderStatus.inDisputeNegotiate].includes(orderStatus) && <div className='card-item-btn short'>
-                    <Button className='antdog-btn disabled' type="primary" disabled>
-                        In dispute
-                    </Button>
-                </div>}
-
-                {/* wait */}
-                {orderStatus === OrderStatus.inDisputeArbitration && <div className='card-item-btn'>
-                    <Button className='antdog-btn disabled' type="primary" disabled>
-                        Platform is in arbitration,please wait
-                    </Button>
-                </div>}
-
-                {/* Completed */}
-                {orderStatus === OrderStatus.completed && <div className='card-item-btn short'>
-                    <Button className='antdog-btn disabled' type="primary" disabled>
-                        Order Completed
-                    </Button>
-                </div>}
-            </>
-            )}
-
         </div >
     )
 }
